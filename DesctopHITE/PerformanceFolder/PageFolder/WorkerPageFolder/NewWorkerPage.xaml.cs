80bd65c0f7f1cf7a33eb904ccd7578c09c3a5466
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-03-19 22:48:38 +0300

    sadasd

--- DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/NewWorkerPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/NewWorkerPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Net.Mail;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
