0b9994dfab5df1663bb2300b182a51b2f0655fc4
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-03-20 16:22:24 +0300

    asdasd

--- DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/ListWorkerPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/ListWorkerPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,10 +26,53 @@ namespace DesctopHITE.PerformanceFolder.PageFolder.WorkerPageFolder
             AppConnectClass.DataBase = new DesctopHiteEntities();
             ListWorkerListBox.ItemsSource = AppConnectClass.DataBase.WorkerTabe.ToList();
         }
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                EditButton.IsEnabled = false;
+                DeliteButton.IsEnabled = false;
+            }
+        }
+        #region Click
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+
+        private void DeliteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+        #region SelectionChanged_MouseDoubleClick
         private void ListWorkerListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
         }
+        private void ListWorkerListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            EditButton.IsEnabled = true;
+            DeliteButton.IsEnabled = true;
+        }
+        private void SearchTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (SearchTextBox.Text == "")
+            {
+                HintSearchTextBlock.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                HintSearchTextBlock.Visibility = Visibility.Collapsed;
+                var Sweep = AppConnectClass.DataBase.WorkerTabe.Include(Blood => Blood.PassportTable).ToList();
+                Sweep = Sweep.Where(Cookie =>
+                Cookie.PassportTable.Surname_Passport.ToLower().Contains(SearchTextBox.Text.ToLower()) ||
+                Cookie.PassportTable.Name_Passport.ToLower().Contains(SearchTextBox.Text.ToLower()) ||
+                Cookie.PassportTable.Middlename_Passport.ToLower().Contains(SearchTextBox.Text.ToLower())) .ToList();
+
+                ListWorkerListBox.ItemsSource = Sweep.ToList();
+            }
+        }
+        #endregion
     }
 }
--- DesctopHITE/PerformanceFolder/UserControlFolder/ListWorkerControl.xaml.cs
+++ DesctopHITE/PerformanceFolder/UserControlFolder/ListWorkerControl.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using DesctopHITE.AppDateFolder.ClassFolder;
+using DesctopHITE.AppDateFolder.ModelFolder;
 
 namespace DesctopHITE.PerformanceFolder.UserControlFolder
 {
@@ -20,10 +13,20 @@ namespace DesctopHITE.PerformanceFolder.UserControlFolder
         public ListWorkerControl()
         {
             InitializeComponent();
-            if (UserImage.Source == null) 
+            AppConnectClass.DataBase = new DesctopHiteEntities();
+        }
+
+        private void UserControl_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
             {
-                ImageSource userImage = new BitmapImage(new Uri("/ContentFolder/ImageFolder/NoImage.png", UriKind.RelativeOrAbsolute)); // Вывод стандартного фото
-                UserImage.Source = userImage;
+                PassportTable passportTable = new PassportTable();
+
+                if (passportTable.Image_Passport == null)
+                {
+                    ImageSource userImage = new BitmapImage(new Uri("/ContentFolder/ImageFolder/NoImage.png", UriKind.RelativeOrAbsolute)); // Вывод стандартного фото
+                    UserImage.Source = userImage;
+                }
             }
         }
     }
