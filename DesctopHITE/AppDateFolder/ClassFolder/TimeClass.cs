6114606f0ee89986df74ce9c7cfc34ad106e84dc
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-04-01 21:56:53 +0300

    фывфыв

--- DesctopHITE/AppDateFolder/ClassFolder/TimeClass.cs
+++ DesctopHITE/AppDateFolder/ClassFolder/TimeClass.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿//----------------------------------------------------------------------------------------------------------
+// В данном классе идёт работа с системной датой и временем
+// В зависимости от даты или времени, в переменные засовывается определённый текст.
+// Переменные в последствии вызываются
+//----------------------------------------------------------------------------------------------------------
+
+using System;
 
 namespace DesctopHITE.AppDateFolder.ClassFolder
 {
     public partial class TimeClass
     {
         DateTime ToDay = DateTime.Now;
-        public string WhatTimeIsIt // В зависимости от текущего времени, выводим приветствие
+        public string WhatTimeIsIt // В зависимости от текущего времени, выводим приветствие (Смотрим, какой сейчас час)
         {
             get
             {
@@ -37,7 +38,7 @@ namespace DesctopHITE.AppDateFolder.ClassFolder
             }
         }
 
-        public string WhatDayIsIt // В зависимости от текущей даты, выводим приветствие
+        public string WhatDayIsIt // В зависимости от текущей даты, выводим приветствие (Смотрим, какой сегодня праздник)
         {
             get
             {
@@ -71,14 +72,34 @@ namespace DesctopHITE.AppDateFolder.ClassFolder
                 {
                     TitleNowDay = "С днём смеха!";
                 }
-                ////else if (ToDay.Day >= 1 && ToDay.Month == 1)
-                ////{
-                ////    TitleNowDay = "";
-                ////}
-                ////else if (ToDay.Day >= 1 && ToDay.Month == 1)
-                ////{
-                ////    TitleNowDay = "";
-                ////}
+                else if (ToDay.Day == 12 && ToDay.Month == 4)
+                {
+                    TitleNowDay = "С днём космонавтики!";
+                }
+                else if (ToDay.Day == 1 && ToDay.Month == 5)
+                {
+                    TitleNowDay = "С Праздником Весны и Труда";
+                }
+                else if (ToDay.Day == 9 && ToDay.Month == 5)
+                {
+                    TitleNowDay = "С Днём Победы";
+                }
+                else if (ToDay.Day == 1 && ToDay.Month == 6)
+                {
+                    TitleNowDay = "С Международным днём защиты детей!";
+                }
+                else if (ToDay.Day == 12 && ToDay.Month == 6)
+                {
+                    TitleNowDay = "С днем России!";
+                }
+                else if (ToDay.Day == 1 && ToDay.Month == 9)
+                {
+                    TitleNowDay = "С днем Знаний!";
+                }
+                else if (ToDay.Day == 4 && ToDay.Month == 11)
+                {
+                    TitleNowDay = "С днем народного единства!";
+                }
                 return TitleNowDay;
             }
         }
