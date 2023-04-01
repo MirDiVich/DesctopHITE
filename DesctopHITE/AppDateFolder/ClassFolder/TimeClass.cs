8d96d4381b0297fcfe2fb1f8c1d748712fb40c2a
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-04-01 20:43:49 +0300

    asdasd

--- DesctopHITE/AppDateFolder/ClassFolder/TimeClass.cs
+++ DesctopHITE/AppDateFolder/ClassFolder/TimeClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DesctopHITE.AppDateFolder.ClassFolder
 {
@@ -18,6 +19,14 @@ namespace DesctopHITE.AppDateFolder.ClassFolder
                 if (ToDay.Hour >= 6 && ToDay.Hour < 12)
                 {
                     TitleToDay = "Доброе утро!";
+                }
+                else if (ToDay.Hour >= 12 && DateTime.Now.Hour < 18)
+                {
+                    TitleToDay = "Доброе день!";
+                }
+                else if (ToDay.Hour >= 18 && DateTime.Now.Hour < 23)
+                {
+                    TitleToDay = "Доброе вечер!";
                 }
 
                 return TitleToDay;
