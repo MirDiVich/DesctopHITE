8da75615c179f52d05d28817e2e03785a91cde43
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-04-17 02:30:55 +0300

    фцв

--- DesctopHITE/PerformanceFolder/WindowsFolder/CaptchaWindow.xaml.cs
+++ DesctopHITE/PerformanceFolder/WindowsFolder/CaptchaWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿///----------------------------------------------------------------------------------------------------------
-/// В данном окне реализован код капчи
-/// Текст капчи генерируется рандомно
-/// Свойство текста капчи принимаются из класса
+/// В данном окне реализован код капчи;
+/// Текст капчи генерируется рандомно;
+/// Свойство текста капчи принимаются из класса;
 ///----------------------------------------------------------------------------------------------------------
 
 using DesctopHITE.AppDateFolder.ClassFolder;
@@ -17,50 +17,57 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
 {
     public partial class CaptchaWindow : Window
     {
-        string GetCaptchaText;
-        string MessageNullBox;
-        DispatcherTimer GetTimer;
+        string getCaptchaText;
+        string messageNullBox;
+        DispatcherTimer getTimer;
 
         public CaptchaWindow()
         {
             InitializeComponent();
         }
+
         private void Window_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (Visibility == Visibility.Visible)
             {
+                // Вызов методов
                 RandomGeneratedCaptcha();
                 GetStyleCaptcha();
 
                 // Свойства для Таймера
-                GetTimer = new DispatcherTimer(); 
-                GetTimer.Tick += new EventHandler(GetTimer_Tick); 
-                GetTimer.Interval = TimeSpan.FromSeconds(5); 
-                GetTimer.Stop();
+                getTimer = new DispatcherTimer(); 
+                getTimer.Tick += new EventHandler(GetTimer_Tick); 
+                getTimer.Interval = TimeSpan.FromSeconds(5); 
+                getTimer.Stop();
             }
             else
             {
                 NewCaptchaButton.IsEnabled = true;
-                GetTimer.Stop();
+                getTimer.Stop();
             }
         }
+
+        #region Метод
+
         private void GetTimer_Tick(object sender, EventArgs e) // Действие, которое будет происходит в определённый промежуток времени
         {
             NewCaptchaButton.IsEnabled = true;
-            GetTimer.Stop();
+            getTimer.Stop();
         }
-        #region Метод
+
         private void ErrorNullBox() // Метод проверки текстового поля на пустоту
         {
-            if (string.IsNullOrWhiteSpace(CaptchaTextBox.Text)) MessageNullBox += "Поле 'Капча' пустое\n";
+            if (string.IsNullOrWhiteSpace(CaptchaTextBox.Text)) messageNullBox += "Поле 'Капча' пустое";
         }
+
         private void GetEnter() // Вызываемый метод для проверки введённой капчи
         {
+            // Вызов метода
             ErrorNullBox();
 
-            if (MessageNullBox == null)
+            if (messageNullBox == null)
             {
-                if (CaptchaTextBox.Text == GetCaptchaText)
+                if (CaptchaTextBox.Text == getCaptchaText)
                 {
                     this.Close();
                 }
@@ -69,7 +76,10 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
                     MessageBox.Show(
                         "То, что вы ввели, не совпадает с Капчей", "Капча",
                         MessageBoxButton.OK, MessageBoxImage.Warning);
+
                     CaptchaTextBox.Text = "";
+
+                    // Вызов методов
                     RandomGeneratedCaptcha();
                     GetStyleCaptcha();
                 }
@@ -77,11 +87,13 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
             else
             {
                 MessageBox.Show(
-                    MessageNullBox, "Капча",
+                    messageNullBox, "Капча",
                     MessageBoxButton.OK, MessageBoxImage.Error);
-                MessageNullBox = null;
+
+                messageNullBox = null;
             }
         }
+
         public void RandomGeneratedCaptcha() // Метод для рандомного содержимого TextCaptcha 
         {
             Random textRandom = new Random();
@@ -99,27 +111,29 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
             // Генератор самой капчи
             string CaptchaText = new string(Enumerable.Repeat(charText, lengthCaptcha).Select(s => s[textRandom.Next(s.Length)]).ToArray());
 
-            GetCaptchaText = CaptchaText;
-            TextCaptchaTextBlock.Text = GetCaptchaText;
+            getCaptchaText = CaptchaText;
+            TextCaptchaTextBlock.Text = getCaptchaText;
         }
 
         private void GetStyleCaptcha() // Метод для рандомного свойства TextCaptcha 
         {
             // Обращение к классу
-            var StyleText = CaptchaClass.GetStyle();
+            var styleText = CaptchaClass.GetStyle();
 
             // Присваивание TextCaptchaTextBlock определённых свойст из класса
-            TextCaptchaTextBlock.Opacity = StyleText.GetOpacityText;
-            TextCaptchaTextBlock.Foreground = StyleText.GetColorText;
-            TextCaptchaTextBlock.FontSize = StyleText.GetFontSizeText;
-            TextCaptchaTextBlock.RenderTransform = new RotateTransform(StyleText.GetRotationText);
-            TextCaptchaTextBlock.RenderTransform = new SkewTransform(0, Math.Sin(DateTime.Now.Millisecond / StyleText.GetFrequencyText) * StyleText.GetAmplitudeText);
-            TextCaptchaTextBlock.FontWeight = StyleText.GetFontWeightText;
-            TextCaptchaTextBlock.FontStyle = StyleText.GetFontStyleText;
-            TextCaptchaTextBlock.TextDecorations = StyleText.GetTextDecorationText;
-        }
+            TextCaptchaTextBlock.Opacity = styleText.GetOpacityText;
+            TextCaptchaTextBlock.Foreground = styleText.GetColorText;
+            TextCaptchaTextBlock.FontSize = styleText.GetFontSizeText;
+            TextCaptchaTextBlock.RenderTransform = new RotateTransform(styleText.GetRotationText);
+            TextCaptchaTextBlock.RenderTransform = new SkewTransform(0, Math.Sin(DateTime.Now.Millisecond / styleText.GetFrequencyText) * styleText.GetAmplitudeText);
+            TextCaptchaTextBlock.FontWeight = styleText.GetFontWeightText;
+            TextCaptchaTextBlock.FontStyle = styleText.GetFontStyleText;
+            TextCaptchaTextBlock.TextDecorations = styleText.GetTextDecorationText;
+        }   
+
         #endregion
         #region Click
+
         private void NewCaptchaButton_Click(object sender, RoutedEventArgs e) // Сгенерировать новую капчу
         {
             // Вызов методов
@@ -128,12 +142,12 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
 
             // Отключение кнопки, чтоб пользователь не "спамил"
             NewCaptchaButton.IsEnabled = false; 
-            GetTimer.Start();
+            getTimer.Start();
         }
 
         private void EnterCaptchaButton_Click(object sender, RoutedEventArgs e) // Нажатие кнопка "ПРОДОЛЖИТЬ"
         {
-            // Вызов методов
+            // Вызов метода
             GetEnter();
         }
 
@@ -141,12 +155,14 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
         {
             if (e.Key == Key.Enter)
             {
-                // Вызов методов
+                // Вызов метода
                 GetEnter();
             }
         }
+
         #endregion
         #region Показать\Скрыть текстовую подсказку
+
         private void CaptchaTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (CaptchaTextBox.Text.Length > 0)
@@ -158,11 +174,7 @@ namespace DesctopHITE.PerformanceFolder.WindowsFolder
                 HintCaptchaTextBlock.Visibility = Visibility.Visible;
             }
         }
-        #endregion
 
-        /// TODO Капча (головоломка):
-        /// Было бы не плохо, добавить несколько простых головоломок,
-        /// с которыми пользователь справится легко и быстро.
-        /// Головоломки естественно на рандом
+        #endregion
     }
 }
