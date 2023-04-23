97fdaaa1a1a135234ca5e23c213edcd84b55e648
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-04-23 22:15:52 +0300

    ол

--- DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/ViewInformationWorkerPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/ViewInformationWorkerPage.xaml.cs
@@ -1,19 +1,13 @@
-﻿using DesctopHITE.AppDateFolder.ClassFolder;
+﻿///----------------------------------------------------------------------------------------------------------
+/// Данная страница нужна для просмотра полной информации о сотруднике;
+/// Если пользователь захочет изменить какую либо информацию о пользователе, то он может
+///     сделать это просто нажав по кнопке "Редактировать".
+///----------------------------------------------------------------------------------------------------------
+
+using DesctopHITE.AppDateFolder.ClassFolder;
 using DesctopHITE.AppDateFolder.ModelFolder;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DesctopHITE.PerformanceFolder.PageFolder.WorkerPageFolder
 {
@@ -43,96 +37,53 @@ namespace DesctopHITE.PerformanceFolder.PageFolder.WorkerPageFolder
         }
 
         #region Click
-
+        // Так как код очень простой и короткий, было принято решение написать его в "длину"
         private void PassportToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (PassportToggleButton.IsChecked == true)
-            {
-                PassportBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                PassportBorder.Visibility = Visibility.Collapsed;
-            }
+            if (PassportToggleButton.IsChecked == true) { PassportBorder.Visibility = Visibility.Visible; }
+            else { PassportBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void PlaceResidenceToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (PlaceResidenceToggleButton.IsChecked == true)
-            {
-                PlaceResidenceBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                PlaceResidenceBorder.Visibility = Visibility.Collapsed;
-            }
+            if (PlaceResidenceToggleButton.IsChecked == true) { PlaceResidenceBorder.Visibility = Visibility.Visible; }
+            else { PlaceResidenceBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void MedicalBookToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (MedicalBookToggleButton.IsChecked == true)
-            {
-                MedicalBookBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                MedicalBookBorder.Visibility = Visibility.Collapsed;
-            }
+            if (MedicalBookToggleButton.IsChecked == true) { MedicalBookBorder.Visibility = Visibility.Visible; }
+            else { MedicalBookBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void SnilsToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (SnilsToggleButton.IsChecked == true)
-            {
-                SnilsBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                SnilsBorder.Visibility = Visibility.Collapsed;
-            }
+            if (SnilsToggleButton.IsChecked == true) { SnilsBorder.Visibility = Visibility.Visible; }
+            else { SnilsBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void INNToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (INNToggleButton.IsChecked == true)
-            {
-                INNBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                INNBorder.Visibility = Visibility.Collapsed;
-            }
+            if (INNToggleButton.IsChecked == true) { INNBorder.Visibility = Visibility.Visible; }
+            else { INNBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void SalaryCardToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (SalaryCardToggleButton.IsChecked == true)
-            {
-                SalaryCardBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                SalaryCardBorder.Visibility = Visibility.Collapsed;
-            }
+            if (SalaryCardToggleButton.IsChecked == true) { SalaryCardBorder.Visibility = Visibility.Visible; }
+            else { SalaryCardBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void GeneralDataToggleButton_Click(object sender, RoutedEventArgs e)
         {
-            if (GeneralDataToggleButton.IsChecked == true)
-            {
-                GeneralDataBorder.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                GeneralDataBorder.Visibility = Visibility.Collapsed;
-            }
+            if (GeneralDataToggleButton.IsChecked == true) { GeneralDataBorder.Visibility = Visibility.Visible; }
+            else { GeneralDataBorder.Visibility = Visibility.Collapsed; }
         }
 
         private void EditWorkerButton_Click(object sender, RoutedEventArgs e)
         {
             FrameNavigationClass.ViewEditInformationWorker_FNC.Navigate(new NewWorkerPage(dataContextWorker));
         }
-
         #endregion
     }
 }
