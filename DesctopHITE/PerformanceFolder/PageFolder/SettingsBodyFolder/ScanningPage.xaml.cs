5afab78e59ca9a2436a181eab6582fe8dfd87479
Миронов Владислав <vlad.banana.syca@yandex.ru>
2023-04-30 20:09:19 +0300

    ыпоывал

--- DesctopHITE/PerformanceFolder/PageFolder/SettingsBodyFolder/ScanningPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/SettingsBodyFolder/ScanningPage.xaml.cs
@@ -3,16 +3,20 @@
 ///----------------------------------------------------------------------------------------------------------
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace DesctopHITE.PerformanceFolder.PageFolder.SettingsBodyFolder
 {
     public partial class ScanningPage : Page
     {
+
         public ScanningPage()
         {
             InitializeComponent();
@@ -20,7 +24,50 @@ namespace DesctopHITE.PerformanceFolder.PageFolder.SettingsBodyFolder
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (TestButton.IsChecked == true)
+            {
+                TestButton.Content = "Отменить";
+                TestButton.Width = 220;
+
+                ProgressScanTextBlock.Visibility = Visibility.Visible;
+
+                CreatingFalseScan();
+            }
+            else
+            {
+                TestButton.Content = "Запустить сканирование системы";
+                TestButton.Width = 360;
+
+                ProgressScanTextBlock.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private CancellationTokenSource cancellationTokenSource;
+
+        private async void CreatingFalseScan()
+        {
+            // если предыдущее сканирование еще не завершено, отменяем его
+            if (cancellationTokenSource != null && !cancellationTokenSource.IsCancellationRequested)
+            {
+                cancellationTokenSource.Cancel();
+            }
+
+            cancellationTokenSource = new CancellationTokenSource();
+
+            Random randomScan = new Random();
+            var TimeScan = randomScan.Next(10, 121);
+            int sleepTime = TimeScan / 100;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (cancellationTokenSource.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                ProgressScanTextBlock.Text = $"{i}%";
+                await Task.Delay(sleepTime * 1000, cancellationTokenSource.Token);
+            }
         }
     }
 }
