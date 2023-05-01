d88f57d369f2dd8e4018574d322319c7752820d2
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-05-02 01:36:08 +0300

    asdasd

--- DesctopHITE/PerformanceFolder/PageFolder/SettingsBodyFolder/ScanningPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/SettingsBodyFolder/ScanningPage.xaml.cs
@@ -2,55 +2,152 @@
 /// На данной странице реализован код для проведения тестов приложения пользователем
 ///----------------------------------------------------------------------------------------------------------
 
+using DesctopHITE.AppDateFolder.ClassFolder;
+using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace DesctopHITE.PerformanceFolder.PageFolder.SettingsBodyFolder
 {
     public partial class ScanningPage : Page
     {
-        DispatcherTimer timer;
-        int timeScan;
+        private int targetTime;
+        private DateTime startTime;
+        private DispatcherTimer dispatcherTimer;
 
         public ScanningPage()
         {
             InitializeComponent();
+
+            // Работа с таймером
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+            dispatcherTimer.Tick += Timer_Tick;
         }
 
-        private void TestButton_Click(object sender, RoutedEventArgs e)
+        #region Click
+        private void CheckScanButton_Click(object sender, RoutedEventArgs e)
         {
-            if (TestButton.IsChecked == true)
+            if (CheckScanButton.IsChecked == true)
             {
-                TestButton.Content = "Отменить";
-                TestButton.Width = 220;
+                StartLoadingAnimation();
+                ScanDeception();
 
-                ProgressScanTextBlock.Visibility = Visibility.Visible;
-
-                //CreatingFalseScan();
+                CheckScanButton.Content = "Остановить";
+                ProgressScanTextBlock.Text = "0%";
             }
             else
             {
-                TestButton.Content = "Запустить сканирование системы";
-                TestButton.Width = 360;
+                StopLoadingAnimation();
 
-                ProgressScanTextBlock.Visibility = Visibility.Collapsed;
+                dispatcherTimer.Stop();
+                CheckScanButton.Content = "Проверить обновление";
+                ProgressScanTextBlock.Text = "///";
+            }
+        }
+        #endregion
+        #region Метод
+        private void StartLoadingAnimation() // Запуск анимации
+        {
+            // Задаю свойства для элементов
+            LoadingCircle0.RenderTransformOrigin = new Point(0.5, 0.5);
+            LoadingCircle0.RenderTransform = new RotateTransform();
+
+            LoadingCircle1.RenderTransformOrigin = new Point(0.5, 0.5);
+            LoadingCircle1.RenderTransform = new RotateTransform();
+
+            LoadingCircle2.RenderTransformOrigin = new Point(0.5, 0.5);
+            LoadingCircle2.RenderTransform = new RotateTransform();
+
+            LoadingCircle3.RenderTransformOrigin = new Point(0.5, 0.5);
+            LoadingCircle3.RenderTransform = new RotateTransform();
+
+            // Задаю свойства для анимации
+            DoubleAnimation animationLeft = new DoubleAnimation();
+            animationLeft.From = 0;
+            animationLeft.To = 360;
+            animationLeft.Duration = new Duration(TimeSpan.FromSeconds(2));
+            animationLeft.RepeatBehavior = RepeatBehavior.Forever;
+
+            DoubleAnimation animationRight = new DoubleAnimation();
+            animationRight.From = 0;
+            animationRight.To = -360;
+            animationRight.Duration = new Duration(TimeSpan.FromSeconds(4));
+            animationRight.RepeatBehavior = RepeatBehavior.Forever;
+
+            // Подключаю анимацию
+            LoadingCircle0.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, animationLeft);
+            LoadingCircle1.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, animationRight);
+
+            // Меняю свойство анимации и подключаю её
+            animationLeft = new DoubleAnimation();
+            animationLeft.From = 0;
+            animationLeft.To = 360;
+            animationLeft.Duration = new Duration(TimeSpan.FromSeconds(6));
+            animationLeft.RepeatBehavior = RepeatBehavior.Forever;
+            LoadingCircle2.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, animationLeft);
+
+            animationRight = new DoubleAnimation();
+            animationRight.From = 0;
+            animationRight.To = -360;
+            animationRight.Duration = new Duration(TimeSpan.FromSeconds(8));
+            animationRight.RepeatBehavior = RepeatBehavior.Forever;
+            LoadingCircle3.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, animationRight);
+        }
+
+        private void StopLoadingAnimation() // Остановка анимации
+        {
+            LoadingCircle0.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+            LoadingCircle1.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+            LoadingCircle2.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+            LoadingCircle3.RenderTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+        }
+
+        private void ScanDeception()
+        {
+            // Получаю рандомное время
+            Random random = new Random();
+            targetTime = random.Next(10, 181);
+            startTime = DateTime.Now;
+
+            // создание и запуск таймера
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += Timer_Tick;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // определение интервала времени в процентах
+            int elapsedSeconds = (int)(DateTime.Now - startTime).TotalSeconds;
+            int percentage = (int)(elapsedSeconds / (float)targetTime * 100);
+
+            if (percentage >= 100)
+            {
+                StopLoadingAnimation();
+
+                dispatcherTimer.Stop();
+                ProgressScanTextBlock.Text = "100%";
+            }
+            else
+            {
+                ProgressScanTextBlock.Text = $"{percentage}%";
             }
         }
 
-        //private void CreatingFalseScan()
-        //{
-        //    Random randomScan = new Random();
-        //    timeScan = randomScan.Next(20, 151);
+        private void ReceivingDataWaitingForStorage() // Вывод информации о том, кто последний раз проверял обновления приложения
+        {
+           
+        }
 
-        //    timer.Interval = TimeSpan.FromSeconds(timeScan);
-        //    timer.Tick += Timer_Tick;
-        //    timer.Start();
-        //}
-
-        //private void Timer_Tick(object sender, EventArgs e)
-        //{
-        //    ProgressScanTextBlock.Text = $"{timeScan}";
-        //}
+        private void OutputDataWaitingForStorage() // Фиксация информации о том, кто последний раз проверял обновления приложения
+        {
+            
+        }
+        #endregion
     }
 }
