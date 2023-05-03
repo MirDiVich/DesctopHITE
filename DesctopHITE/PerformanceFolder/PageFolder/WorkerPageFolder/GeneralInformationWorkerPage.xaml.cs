58435d0e3194f2d82428c00db690e85fd462ff09
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-05-03 21:40:17 +0300

    Почистил код в GeneralInformationWorkerPage.cs

--- DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/GeneralInformationWorkerPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/GeneralInformationWorkerPage.xaml.cs
@@ -21,91 +21,155 @@ namespace DesctopHITE.PerformanceFolder.PageFolder.WorkerPageFolder
 
         public GeneralInformationWorkerPage()
         {
-            InitializeComponent();
-            AppConnectClass.DataBase = new DesctopHiteEntities();
+            try
+            {
+                InitializeComponent();
+                AppConnectClass.DataBase = new DesctopHiteEntities();
+            }
+            catch (Exception ex) 
+            {
+                var nameMessageOne = $"Ошибка (GE - 001)";
+                var titleMessageOne = $"{ex.Message}";
+                MessageBox.Show(
+                    nameMessageOne, titleMessageOne,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
         }
 
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            if (Visibility == Visibility.Visible)
+            try
             {
-                GetDataCountWorker();
-                GetBirthdayComingSoonWorker();
-                GetTodayBirthdayWorker();
-                GetGenderWorker();
-                GetNumberEmployeesWithPosition();
+                if (Visibility == Visibility.Visible)
+                {
+                    GetDataCountWorker();
+                    GetBirthdayComingSoonWorker();
+                    GetTodayBirthdayWorker();
+                    GetGenderWorker();
+                    GetNumberEmployeesWithPosition();
+                }
+            }
+            catch (Exception exVisibility)
+            {
+                var nameMessageVisibility = $"Ошибка (GE - 002)";
+                var titleMessageVisibility = $"{exVisibility.Message}";
+                MessageBox.Show(
+                    nameMessageVisibility, titleMessageVisibility,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
             }
         }
 
+        #region Метод
         private void GetDataCountWorker() // Считаю количество сотрудников / количество онлайн / оффлайн
         {
-            var dataCountWorker = AppConnectClass.DataBase.WorkerTable;
+            try
+            {
+                var dataCountWorker = AppConnectClass.DataBase.WorkerTable;
 
-            WorkerCountTextBlock.Text = $"{dataCountWorker.Count()}";
-            TotalOnlineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 2)}";
-            TotalOfflineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 1)}";
+                WorkerCountTextBlock.Text = $"{dataCountWorker.Count()}";
+                TotalOnlineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 2)}";
+                TotalOfflineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 1)}";
+            }
+            catch (Exception exGetDataCountWorker)
+            {
+                var nameMessageGetDataCountWorker = $"Ошибка (GE - 003)";
+                var titleMessageGetDataCountWorker = $"{exGetDataCountWorker.Message}";
+                MessageBox.Show(
+                    nameMessageGetDataCountWorker, titleMessageGetDataCountWorker,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
         }
 
         private void GetBirthdayComingSoonWorker() // Получаю сотрудников, у которых скоро будет день рождение
         {
-            var www = toDay.AddDays(-5);
+            try
+            {
+                var www = toDay.AddDays(-5);
 
-            // Выведи мне в виде списка только тех сотрудников у которых: день рождение в этом месяце, не сегодня,
-            BirthdayComingSoonListView.ItemsSource = 
-                AppConnectClass.DataBase.PassportTable.Where(dateOfBrith =>
-                    dateOfBrith.DateOfBrich_Passport.Month == toDay.Month &&
-                    dateOfBrith.DateOfBrich_Passport.Day != toDay.Day &&
-                    dateOfBrith.DateOfBrich_Passport <= www).ToList();
+                // Выведи мне в виде списка только тех сотрудников у которых: день рождение в этом месяце, не сегодня,
+                BirthdayComingSoonListView.ItemsSource =
+                    AppConnectClass.DataBase.PassportTable.Where(dateOfBrith =>
+                        dateOfBrith.DateOfBrich_Passport.Month == toDay.Month &&
+                        dateOfBrith.DateOfBrich_Passport.Day != toDay.Day &&
+                        dateOfBrith.DateOfBrich_Passport <= www).ToList();
 
-            /// Вся проблема заключается в том, что он берёт всех сотрудников, которые
-            /// соответствуют условию, а условие следующее:
-            /// возьми всех сотрудников и выведи только тех, кто соответствует следующему условию (25.04.2023 - 5 = 20.04.2023) <= 25.04.2023
-            /// Результат такой, ок, я выведу тебе сотрудников, у которых дени рождение раньше или попало в следующию дату "20.04.2023",
-            /// а я пытаюсь написать условие, на вывод тольел тех, у кого через 5 дней день рождение.
+                /// Вся проблема заключается в том, что он берёт всех сотрудников, которые
+                /// соответствуют условию, а условие следующее:
+                /// возьми всех сотрудников и выведи только тех, кто соответствует следующему условию (25.04.2023 - 5 = 20.04.2023) <= 25.04.2023
+                /// Результат такой, ок, я выведу тебе сотрудников, у которых дени рождение раньше или попало в следующию дату "20.04.2023",
+                /// а я пытаюсь написать условие, на вывод тольел тех, у кого через 5 дней день рождение.
 
-            /// Поэтому это пока что останется тут. Если этот коммент* не пропал, значит, я ещё не исправил свою проблему!
+                /// Поэтому это пока что останется тут. Если этот коммент* не пропал, значит, я ещё не исправил свою проблему!
+            }
+            catch (Exception exGetBirthdayComingSoonWorker)
+            {
+                var nameMessageGetBirthdayComingSoonWorker = $"Ошибка (GE - 004)";
+                var titleMessageGetBirthdayComingSoonWorker = $"{exGetBirthdayComingSoonWorker.Message}";
+                MessageBox.Show(
+                    nameMessageGetBirthdayComingSoonWorker, titleMessageGetBirthdayComingSoonWorker,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
         }
 
         private void GetTodayBirthdayWorker() // Получаю сотрудников, у которых сегодня день рождение
         {
-            TodayBirthdayListView.ItemsSource = 
-                AppConnectClass.DataBase.PassportTable.Where(toDayBrith =>
-                    toDayBrith.DateOfBrich_Passport.Day == toDay.Day &&
-                    toDayBrith.DateOfBrich_Passport.Month == toDay.Month).ToList();
+            try
+            {
+                TodayBirthdayListView.ItemsSource =
+               AppConnectClass.DataBase.PassportTable.Where(toDayBrith =>
+                   toDayBrith.DateOfBrich_Passport.Day == toDay.Day &&
+                   toDayBrith.DateOfBrich_Passport.Month == toDay.Month).ToList();
+            }
+            catch (Exception exGetTodayBirthdayWorker)
+            {
+                var nameMessageGetTodayBirthdayWorker = $"Ошибка (GE - 005)";
+                var titleMessageGetTodayBirthdayWorker = $"{exGetTodayBirthdayWorker.Message}";
+                MessageBox.Show(
+                    nameMessageGetTodayBirthdayWorker, titleMessageGetTodayBirthdayWorker,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
         }
 
         private void GetGenderWorker() // Считаю количество сотрудников мужского \ женского гендера
         {
-            var genderWorker = AppConnectClass.DataBase.PassportTable;
+            try
+            {
+                var genderWorker = AppConnectClass.DataBase.PassportTable;
 
-            GenderManTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 1)}";
-            GenderWomenTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 2)}";
+                GenderManTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 1)}";
+                GenderWomenTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 2)}";
+            }
+            catch (Exception exGetGenderWorker)
+            {
+                var nameMessageGetGenderWorker = $"Ошибка (GE - 006)";
+                var titleMessageGetGenderWorker = $"{exGetGenderWorker.Message}";
+                MessageBox.Show(
+                    nameMessageGetGenderWorker, titleMessageGetGenderWorker,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
+            }
         }
 
         private void GetNumberEmployeesWithPosition() // Считаю количество сотрудников  по должности
         {
-            var numberEmployeesWithPosition = AppConnectClass.DataBase.WorkerTable;
-
-            NumberEmployeesWithPositionProgrammerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 1)}";
-            NumberEmployeesWithPositionAdministratorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 2)}";
-            NumberEmployeesWithPositionCashierTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 3)}";
-            NumberEmployeesWithPositionCleanerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 4)}";
-            NumberEmployeesWithPositionDirectorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 5)}";
-        }
-
-        private void uhtiur_Click(object sender, RoutedEventArgs e)
-        {
-            DateTime dateTime = DateTime.Today;
             try
             {
-                MessageBox.Show(dateTime.Day.ToString());
-            }
-            catch (Exception ex)
-            {
-                string tututut = $"{ex.Message}";
+                var numberEmployeesWithPosition = AppConnectClass.DataBase.WorkerTable;
 
-                MessageBox.Show(tututut);
+                NumberEmployeesWithPositionProgrammerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 1)}";
+                NumberEmployeesWithPositionAdministratorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 2)}";
+                NumberEmployeesWithPositionCashierTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 3)}";
+                NumberEmployeesWithPositionCleanerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 4)}";
+                NumberEmployeesWithPositionDirectorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 5)}";
+            }
+            catch (Exception exGetNumberEmployeesWithPosition)
+            {
+                var nameMessageetNumberEmployeesWithPosition = $"Ошибка (GE - 007)";
+                var titleMessageetNumberEmployeesWithPosition = $"{exGetNumberEmployeesWithPosition.Message}";
+                MessageBox.Show(
+                    nameMessageetNumberEmployeesWithPosition, titleMessageetNumberEmployeesWithPosition,
+                    MessageBoxButton.OKCancel, MessageBoxImage.Error);
             }
         }
+        #endregion
     }
 }
