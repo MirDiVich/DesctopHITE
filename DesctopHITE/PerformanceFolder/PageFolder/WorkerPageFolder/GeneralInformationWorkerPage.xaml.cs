d71f8ecd39a2fb52aeb2ef1d03382757cf5fe85c
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-05-03 19:17:05 +0300

    ыфва

--- DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/GeneralInformationWorkerPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/WorkerPageFolder/GeneralInformationWorkerPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿///----------------------------------------------------------------------------------------------------------
-/// 
+/// Простая страница пустышка, которая нужна лишь для того, чтоб вывести "общую" информацию о сотрудниках
+///     из базы данных и всё.
 ///----------------------------------------------------------------------------------------------------------
 
 using DesctopHITE.AppDateFolder.ClassFolder;
@@ -22,12 +23,50 @@ namespace DesctopHITE.PerformanceFolder.PageFolder.WorkerPageFolder
         {
             if (Visibility == Visibility.Visible)
             {
-                var CountWorker = AppConnectClass.DataBase.WorkerTable.Count();
-                WorkerCountTextBlock.Text = CountWorker.ToString();
-
-                var CountOnline = AppConnectClass.DataBase.WorkerTable.Count(status => status.pnStatus_Worker == 2);
-                TotalOnlineWorkerTextBlock.Text = CountOnline.ToString();
+                GetDataCountWorker();
+                GetBirthdayComingSoonWorker();
+                GetTodayBirthdayWorker();
+                GetGenderWorker();
+                GetNumberEmployeesWithPosition();
             }
+        }
+
+        private void GetDataCountWorker() // Считаю количество сотрудников / количество онлайн / оффлайн
+        {
+            var dataCountWorker = AppConnectClass.DataBase.WorkerTable;
+
+            WorkerCountTextBlock.Text = $"{dataCountWorker.Count()}";
+            TotalOnlineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 2)}";
+            TotalOfflineWorkerTextBlock.Text = $"{dataCountWorker.Count(status => status.pnStatus_Worker == 1)}";
+        }
+
+        private void GetBirthdayComingSoonWorker() // Получаю сотрудников, у которых скоро будет день рождение
+        {
+
+        }
+
+        private void GetTodayBirthdayWorker() // Получаю сотрудников, у которых сегодня день рождение
+        {
+
+        }
+
+        private void GetGenderWorker() // Считаю количество сотрудников мужского \ женского гендера
+        {
+            var genderWorker = AppConnectClass.DataBase.PassportTable;
+
+            GenderManTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 1)}";
+            GenderWomenTextBlock.Text = $"{genderWorker.Count(gender => gender.pnGender_Passport == 2)}";
+        }
+
+        private void GetNumberEmployeesWithPosition() // Считаю количество сотрудников  по должности
+        {
+            var numberEmployeesWithPosition = AppConnectClass.DataBase.WorkerTable;
+
+            NumberEmployeesWithPositionProgrammerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 1)}";
+            NumberEmployeesWithPositionAdministratorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 2)}";
+            NumberEmployeesWithPositionCashierTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 3)}";
+            NumberEmployeesWithPositionCleanerTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 4)}";
+            NumberEmployeesWithPositionDirectorTextBlock.Text = $"{numberEmployeesWithPosition.Count(role => role.pnRole_Worker == 5)}";
         }
     }
 }
