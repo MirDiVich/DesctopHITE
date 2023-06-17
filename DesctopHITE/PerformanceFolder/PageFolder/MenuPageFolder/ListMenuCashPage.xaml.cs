7290e1ee7cf6ca6dbdda47a41af3e97905f846e8
Bloodthirsty Snowfla <99435523+MaR1XyAnA@users.noreply.github.com>
2023-06-18 00:57:59 +0300

    фыв

--- DesctopHITE/PerformanceFolder/PageFolder/MenuPageFolder/ListMenuCashPage.xaml.cs
+++ DesctopHITE/PerformanceFolder/PageFolder/MenuPageFolder/ListMenuCashPage.xaml.cs
@@ -2,19 +2,9 @@
 using DesctopHITE.AppDateFolder.ModelFolder;
 using DesctopHITE.PerformanceFolder.WindowsFolder;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DesctopHITE.PerformanceFolder.PageFolder.MenuPageFolder
 {
